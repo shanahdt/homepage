b2ab731e806c40aaae4369fb1146ac50d88da539
Daniel Shanahan <daniel.shanahan@gmail.com>
2013-11-03 22:26:40 -0500

    new header

--- source/conf.py
+++ source/conf.py
@@ -149,7 +149,7 @@ html_static_path = ['_static']
 #html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = False
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 html_show_copyright = False
