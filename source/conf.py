189eea9946121f36ae27b73478fcab539a654083
Daniel Shanahan <daniel.shanahan@gmail.com>
2013-11-04 00:26:06 -0500

    new header

--- source/conf.py
+++ source/conf.py
@@ -25,7 +25,11 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+
+extensions = ['sphinxcontrib.googleanalytics']
+googleanalytics_id <string>:
+	UA id for your site, example::
+		googleanalytics_id = 'UA-38564674-1'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
